a7bc3b4824727feed8709fb69b41e2d586a9be47
Hendri Mardani <hendrimardani290301@gmail.com>
2023-03-06 11:52:37 +0700

    cey

--- dq_learning.py
+++ dq_learning.py
@@ -15,6 +15,7 @@ class Robot:
     def __init__(self, env):
         self.env = env
 
+    @property
     def cobaan(self):
         env = gym.make(self.env)
         env.reset()
@@ -27,29 +28,42 @@ class Robot:
 
         return env
     def informasi(self):
-        env = self.cobaan()
+        env = self.cobaan
         state_dims = env.observation_space.shape[0]
         num_actions = env.action_space.n
         return f"Jumlah state = {state_dims}\n Jumlah Actions = {num_actions}"
     
-# class PreprocessEnv(gym.Wrapper):
-#     def __init__(self, env):
-#         gym.Wrapper.__init__(self, env)
+    @property
+    def env_(self):
+        env = gym.make(self.env)
+        env.reset()
 
-#     def reset(self):
-#         state = self.env.reset()
+        return env
         
-#         return torch.from_numpy(state).unsqueeze(dim=0).float()
+class PreprocessEnv(gym.Wrapper):
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+
+    def reset(self):
+        state = self.env.reset()
+        
+        return torch.from_numpy(state).unsqueeze(dim=0).float()
     
-#     def step(self, action):
-#         action = action.item()
-#         next_state, reward, done, _ = self.env.step(action)
-#         next_state = torch.from_numpy(next_state).unsqueeze(dim=0).float()
-#         reward = torch.tensor(reward).view(1, -1).float()
-#         done = torch.tensor(done).view(1, -1)
+    def step(self, action):
+        action = action.item()
+        next_state, reward, done, info = self.env.step(action)
+        next_state = torch.from_numpy(next_state).unsqueeze(dim=0).float()
+        reward = torch.tensor(reward).view(1, -1).float()
+        done = torch.tensor(done).view(1, -1)
 
-#         return next_state, reward, done
+        return next_state, reward, done, info
 
+robot = Robot(env="MountainCar-v0")
 
-hasil = Robot(env="MountainCar-v0")
-print(hasil.informasi())
+env = PreprocessEnv(env=robot.env_)
+action = torch.tensor(0)
+# next_state, reward, done, _ = env.step(action)
+print(env.step(action))
+
+# hasil = Robot(env="MountainCar-v0")
+# print(hasil.informasi())
